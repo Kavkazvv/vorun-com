40edb227ac74b2a1c6132ead6b23c4fb88fb181c
Vorun Kreal <vkreal@gmail.com>
2022-01-04 13:07:18 -0800

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-                { from: "src/images", to: "images" }
+                { from: "src/assets", to: "assets" }
             ]
         })
     ]

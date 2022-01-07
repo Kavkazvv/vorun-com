c86154f45b7f88a558ef8de7aa7699a96c6a996e
Vorun Kreal <vkreal@gmail.com>
2022-01-06 21:35:43 -0800

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <img src="./assets/logo.png" class="center logo" />
+            </div>
+        );
+    }
+}
+export default Header;

85f29e15f59d8a571ad4d847ab09b7e369a3a2f4
Vorun Kreal <vkreal@gmail.com>
2022-01-09 22:48:55 -0800

    AppListItem

--- src/components/AppListItem.js
+++ src/components/AppListItem.js
@@ -6,10 +6,25 @@ class AppListItem extends Component {
         console.log(props.item)
     }
     render() {
-        const {cover} = this.props.item;
+        const { cover, stores } = this.props.item;
         return (
             <div>
-                <img src={cover} />
+                <table align="center">
+                    <tr>
+                        <td>
+                            <img src={cover} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <table width="100%" border="1">
+                            <tr>
+                                {stores.map((item, index) => {
+                                    return <td>1</td>
+                                })}
+                            </tr>
+                        </table>
+                    </tr>
+                </table>
             </div>
         );
     }

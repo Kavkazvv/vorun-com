aea63648d63feb649d367e1ac9a70e367a721381
Vorun Kreal <vkreal@gmail.com>
2022-01-10 20:40:33 -0800

    add Spacer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import AppList from './components/AppList'
+import Spacer from './components/Spacer'
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <Spacer axis="vertical" size={132} />
                 <AppList />
             </div>
         );

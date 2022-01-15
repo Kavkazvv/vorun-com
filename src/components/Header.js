786a904ece7fc17e37c12ff12837828947310551
Vorun Kreal <vkreal@gmail.com>
2022-01-14 17:16:55 -0800

    add social badges

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,24 @@ class Header extends Component {
                 <table width="100%" border="0">
                     <tr>
                         <td align="center">
-                            <img class="header" src="./assets/logo.png" />     
+                            <img class="header" src="./assets/logo.png" />
+                            <br />
+                        </td>
+                    </tr>
+                </table>
+                <table width="100%" border="0">
+                    <tr>
+                        <td align="right">
+                            <a href="https://github.com/vkreal">
+                                <img src="./assets/github.png"
+                                    class="badge" />
+                            </a>
+                        </td>
+                        <td align="left">
+                            <a href="https://www.linkedin.com/in/vkreal/">
+                                <img src="./assets/linkedin.png"
+                                    class="badge" />
+                            </a>
                         </td>
                     </tr>
                 </table>
